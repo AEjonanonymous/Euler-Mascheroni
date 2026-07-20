f9dac93a6d8e4ef2e8edfe0c529c31ce6e493e33
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-07-20 08:06:32 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,8 @@
 import Lake
 open Lake DSL
 
-package "euler-mascheroni"
+package «EulerMascheroni» where
 
 @[default_target]
-lean_lib «Proof_Of_Euler_Mascheroni_Constant_Irrationality»
+lean_lib «EulerMascheroni» where
+  srcDir := "."
