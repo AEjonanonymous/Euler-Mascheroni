e3a2814cf523f54de4e7274af11a89726d6866a5
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-04 23:15:30 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «EulerMascheroni» where
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0"
 
 @[default_target]
 lean_lib «Proof_Of_Euler-Mascheroni_Constant_Irrationality» where
